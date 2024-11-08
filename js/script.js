db3ce44f6d890a95d13d8307b3f8f51a677a11f9
Enrico2002 <167096926+Enrico-02@users.noreply.github.com>
2024-11-08 14:30:15 +0100

    UpdateFix

--- js/script.js
+++ js/script.js
@@ -1,17 +1,41 @@
 
-const counterDisplay = document.getElementById('counter-display');
-const buttons = document.getElementById('buttons');
+const counterWrapper = document.getElementById('counter-wrapper');
 
+
+const counterDisplay = document.createElement('h1');
+counterDisplay.id = 'counter-display';
+counterDisplay.textContent = '0';
+counterWrapper.appendChild(counterDisplay);
+
+
+const buttonsContainer = document.createElement('div');
+buttonsContainer.id = 'buttons';
+counterWrapper.appendChild(buttonsContainer);
+
+
+const decreaseButton = document.createElement('button');
+decreaseButton.id = 'decrease';
+decreaseButton.textContent = '-';
+buttonsContainer.appendChild(decreaseButton);
+
+const resetButton = document.createElement('button');
+resetButton.id = 'reset';
+resetButton.textContent = 'Reset';
+buttonsContainer.appendChild(resetButton);
+
+const increaseButton = document.createElement('button');
+increaseButton.id = 'increase';
+increaseButton.textContent = '+';
+buttonsContainer.appendChild(increaseButton);
 
 let counter = 0;
-
 
 function updateDisplay() {
   counterDisplay.textContent = counter;
 }
 
 
-buttons.addEventListener('click', function(event) {
+buttonsContainer.addEventListener('click', function(event) {
   const clickedButton = event.target.id;
 
   if (clickedButton === 'increase') {
@@ -24,3 +48,4 @@ buttons.addEventListener('click', function(event) {
 
   updateDisplay();
 });
+
