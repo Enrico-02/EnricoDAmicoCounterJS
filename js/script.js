005cc24f24390b751c231a2ee94fe4d44fc92fb4
Enrico2002 <167096926+Enrico-02@users.noreply.github.com>
2024-11-13 15:31:32 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const counterWrapper = document.getElementById('counter-wrapper');
 
-// Funzione per creare elementi del DOM
 function creaElemento(tagName, id = '', textContent = '') {
     const elemento = document.createElement(tagName);
     if (id) elemento.id = id;
@@ -8,20 +7,19 @@ function creaElemento(tagName, id = '', textContent = '') {
     return elemento;
 }
 
-// Creazione del contatore display
+
 const counterDisplay = creaElemento('h1', 'counter-display', '0');
 counterWrapper.appendChild(counterDisplay);
 
-// Creazione del contenitore per i pulsanti
+
 const buttonsContainer = creaElemento('div', 'buttons');
 counterWrapper.appendChild(buttonsContainer);
 
-// Creazione dei pulsanti
+
 const decreaseButton = creaElemento('button', 'decrease', '-');
 const resetButton = creaElemento('button', 'reset', 'Reset');
 const increaseButton = creaElemento('button', 'increase', '+');
 
-// Aggiunta dei pulsanti al contenitore
 buttonsContainer.appendChild(decreaseButton);
 buttonsContainer.appendChild(resetButton);
 buttonsContainer.appendChild(increaseButton);
@@ -32,7 +30,7 @@ function updateDisplay() {
   counterDisplay.textContent = counter;
 }
 
-// Gestore eventi per i pulsanti
+
 buttonsContainer.addEventListener('click', function(event) {
   const clickedButton = event.target.id;
 
